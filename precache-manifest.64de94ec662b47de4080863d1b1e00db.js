826bcef2a1ce3182ae9ab19983d89ad2fd304775
tuong <nguyenhuutuong999@gmail.com>
2019-08-20 14:43:37 +0700

    Updates

---
+++ precache-manifest.64de94ec662b47de4080863d1b1e00db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7328267b31c4e898ec136b77af8310",
+    "url": "/Cart/index.html"
+  },
+  {
+    "revision": "c13947f2c0752ce53c46",
+    "url": "/Cart/static/js/2.16a03d92.chunk.js"
+  },
+  {
+    "revision": "47211d8fe4839d3529ff",
+    "url": "/Cart/static/js/main.182dfa34.chunk.js"
+  },
+  {
+    "revision": "b37afd3443d1f6f83614",
+    "url": "/Cart/static/js/runtime~main.f2839279.js"
+  }
+]);
